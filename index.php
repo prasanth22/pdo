7ef4b0e147a3b02dfccd02142443d3bc2bbe0d0a
prasanth22 <jalendra.velugu@iconma.com>
2020-02-22 12:06:41 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include('config.php');
+if(isset($_POST['insert']))
+
+{
+//getting post values
+	$name=$_POST['fullname'];
+	$phnno=$_POST['phonenumber'];
+	$emailid=$_POST['emailid'];
+	$sql="insert into tbluser(Name,PhoneNumber,Emailid) value(:fname,:phno,:emlid)";
+$query=$con->prepare($sql);
+$query->bindParam(':fname',$name,PDO::PARAM_STR);
+$query->bindParam(':phno',$phnno,PDO::PARAM_INT);
+$query->bindParam(':emlid',$emailid,PDO::PARAM_STR);
+$query->execute();
+$lastinsertid=$con->lastInsertId();
+if($lastinsertid)
+{
+echo "Data Inserted";
+}
+else
+{
+	echo "Something went wrong";
+}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Values using PDO </title>
+</head>
+<body>
+	<p><?php // print_r(PDO::getAvailableDrivers()); //query to check available drivers?></p>
+<form method="post">
+	<h3>Insert data into the database using PDO</h3>
+	Name :<input type="text" name="fullname" required><br />
+	Phone Number <input type="text" name="phonenumber" required><br />
+	Email :<input type="email" name="emailid"  required><br />
+	<input type="submit" name="insert" value="Submit">
+</form>
+</body>
+</html>
---
+++ pdo.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2018 at 02:16 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(120) NOT NULL,
+  `PhoneNumber` int(11) NOT NULL,
+  `Emailid` varchar(150) NOT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`id`, `Name`, `PhoneNumber`, `Emailid`, `PostingDate`) VALUES
+(1, 'Anuj', 423423423, 'anuj@gmail.com', '2018-06-16 12:01:41'),
+(2, 'Test user', 535423, 'test@test.com', '2018-06-16 12:02:07');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
